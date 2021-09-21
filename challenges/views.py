6b7a19291d06bf0aae7507da44a84bdc313ac10a
saddamhr <saddambubt65@gmail.com>
2021-09-21 14:51:37 +0600

    .

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import redirect, render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, response
+from django.shortcuts import  render
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect, response
 from django.urls import reverse
 
 monthly_challenges = {
@@ -14,21 +14,16 @@ monthly_challenges = {
     'september': 'september',
     'october': 'october',
     'november': 'november',
-    'december': 'december',
+    'december': None,
 }
 
+
 def index(request):
-    list_items = ''
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        captalized_month = month.capitalize()
-        month_path = reverse('month-challenge', args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{captalized_month}</a></li>"
-    
-    response_data = f'<ul>{list_items}</ul>'
-
-    return HttpResponse(response_data)
+    return render(request, 'challenges/index.html', {
+        'months': months
+    })
 
 
 def monthly_challenge_by_number(request, month):
@@ -47,8 +42,8 @@ def monthly_challenge(request, month):
         challenge_text = monthly_challenges[month]
 
         return render(request, 'challenges/challenge.html', {
-            'text': challenge_text
+            'text': challenge_text,
+            'month_name': month
         })
     except:
-        return HttpResponseNotFound('<h1>This month is not supported!</h1>')
-        
+        raise Http404()
--- montly_chanllenges/settings.py
+++ montly_chanllenges/settings.py
@@ -58,6 +58,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             # BASE_DIR / 'challenges' / 'templates'
+            BASE_DIR / 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
